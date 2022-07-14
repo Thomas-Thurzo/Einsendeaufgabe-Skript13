39de9c74d7e16e9b60c05e50a76878ad0cd41f94
Thomas Thurzo <thomas.thurzo@gmail.com>
2022-07-14 21:32:17 +0200

    Fertigstellung Aufgabe 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-$(function(){
- $("#submit").on("click", function() {
-    $("#ausgabe").text($("#name").val());
- });
+$(function () {
+
+    // Aufgabe 1 Text Ein- und Ausgabe
+    $("#submit").on("click", function () {
+        $("#ausgabe").text($("#name").val());
+    });
+
+    // Aufgabe 2 Absätze ein- und ausblenden lassen
+    $(".erklaerung").hide();
+    $("h3").on("click", function(){
+        $(this).next().toggle();
+    });
+
 });
